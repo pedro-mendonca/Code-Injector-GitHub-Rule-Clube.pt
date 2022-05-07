a0ac79c8eae6b8c4ce4e2c99f305d1a807e05a16
pedro-mendonca <ped.gaspar@gmail.com>
2022-05-07 16:24:56 +0100

    Melhor responsividade

--- js/analise-composicao-equipas.js
+++ js/analise-composicao-equipas.js
@@ -153,8 +153,8 @@ function paginaEstatistica() {
 
 	/*
 	<div class="row painel-row justify-content-center p-0 m-0">
-		<div class="row col-12 col-sm-10 col-md-8 col-lg-6 painel-score justify-content-center m-3 p-2 card action bg-light text-center">
-			<div class="col team1">
+		<div class="row col-12 col-md-8 col-lg-6 painel-score justify-content-center m-3 p-2 card action bg-light text-center">
+			<div class="col-6 col-sm-4 order-2 order-sm-1 team1">
 				<div class="row d-inline-block">
 					<div class="col-12 h-25 d-inline-block team-posicao posicao-local"></div>
 					<div class="col-12 h-25 d-inline-block team-name font-weight-bold">Paço de Arcos</div>
@@ -162,13 +162,13 @@ function paginaEstatistica() {
 					<div class="col-12 h-25 d-inline-block team-score font-weight-bold">XX</div>
 				</div>
 			</div>
-			<div class="col timer align-self-center">
+			<div class="col-12 col-sm-4 order-1 order-sm-2 timer align-self-center">
 				<div style="" class="row">
 					<div class="col-12 periodo justify-content-center font-weight-bold">P</div>
 					<div class="col-12 countdown-timer justify-content-center font-weight-bold">10:00</div>
 				</div>
 			</div>
-			<div class="col team2">
+			<div class="col-6 col-sm-4 order-3 order-sm-3 team2">
 				<div class="row d-inline-block">
 					<div class="col-12 h-25 d-inline-block team-posicao"></div>
 					<div class="col-12 h-25 d-inline-block team-name font-weight-bold">Adversário</div>
@@ -179,7 +179,7 @@ function paginaEstatistica() {
 		</div>
 	</div>
 	*/
-	$( 'div#chrono' ).before( '<div class="row painel-row justify-content-center p-0 m-0"><div class="row col-12 col-sm-10 col-md-8 col-lg-6 painel-score justify-content-center m-3 p-2 card action bg-light text-center"><div class="col team1"><div class="row d-inline-block"><div class="col-12 h-25 d-inline-block team-posicao posicao-local"></div><div class="col-12 h-25 d-inline-block team-name font-weight-bold">Paço de Arcos</div><div class="col-12 h-25 d-inline-block team-escalao"></div><div class="col-12 h-25 dinlineblock team-score font-weight-bold">XX</div></div></div><div class="col timer align-self-center"><div style="" class="row"><div class="col-12 periodo justify-content-center font-weight-bold">P</div><div class="col-12 countdown-timer justify-content-center font-weight-bold">10:00</div></div></div><div class="col team2"><div class="row d-inline-block"><div class="col-12 h-25 d-inline-block team-posicao"></div><div class="col-12 h-25 d-inline-block team-name font-weight-bold">Adversário</div><div class="col-12 h-25 d-inline-block team-escalao"></div><div class="col-12 h-25 d-inline-block team-score font-weight-bold">XX</div></div></div></div></div>' );
+	$( 'div#chrono' ).before( '<div class="row painel-row justify-content-center p-0 m-0"><div class="row col-12 col-md-8 col-lg-6 painel-score justify-content-center m-3 p-2 card action bg-light text-center"><div class="col-6 col-sm-4 order-2 order-sm-1 team1"><div class="row d-inline-block"><div class="col-12 h-25 d-inline-block team-posicao posicao-local"></div><div class="col-12 h-25 d-inline-block team-name font-weight-bold">Paço de Arcos</div><div class="col-12 h-25 d-inline-block team-escalao"></div><div class="col-12 h-25 dinlineblock team-score font-weight-bold">XX</div></div></div><div class="col-12 col-sm-4 order-1 order-sm-2 timer align-self-center"><div style="" class="row"><div class="col-12 periodo justify-content-center font-weight-bold">P</div><div class="col-12 countdown-timer justify-content-center font-weight-bold">10:00</div></div></div><div class="col-6 col-sm-4 order-3 order-sm-3 team2"><div class="row d-inline-block"><div class="col-12 h-25 d-inline-block team-posicao"></div><div class="col-12 h-25 d-inline-block team-name font-weight-bold">Adversário</div><div class="col-12 h-25 d-inline-block team-escalao"></div><div class="col-12 h-25 d-inline-block team-score font-weight-bold">XX</div></div></div></div></div>' );
 
 	// Capta nomes das equipas em <font>PAC<span>Adversário</font>.
 	var score = $( 'div#score font' ).html();
